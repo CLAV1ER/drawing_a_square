7035fdd57054eb530bb613d8f999453598550acf
Genji Shikama <gshikama@gmail.com>
2015-10-08 19:38:38 -0400

    drawing_a_square.py
    Using turtles to draw a shapes
    (Udacity exercise)

---
+++ drawing_a_square.py
@@ -0,0 +1,28 @@
+# Drawing square
+
+import turtle
+def draw_square(some_turle):
+	for i in range (1,5)
+	some_turtle.forward(100)
+	someturtle.right(90)
+
+def draw_art():
+	windows = turtle.screen()
+	window.bgcolor("red")
+	# create the turtle brad - draw squares
+	brad = turtle.turtle()
+	brad.shape("turtle")
+	brad.color("yellow")
+	brad.speed.(2)
+	for i in range(1,37):
+		draw_square(brad)
+		brad.right(10)
+	# create the turtle angie - drawa circles
+	angie.turtle.Turtle()
+	angie.shape("arrove")
+	angie.color("blue")
+	angie.circle(100)
+
+	window.exitonclick()
+
+draw_art()
